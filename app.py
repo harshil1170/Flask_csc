1c2685e8aa8775aa8b8520f6d47e0488dc3c755b
Jamnesha Harshil <131521283+harshil1170@users.noreply.github.com>
2025-07-04 22:28:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("Home.html")
+    
+print("Current working directory:", os.getcwd())
+print("Templates folder contents:", os.listdir('templates'))
 
 if __name__ == '__main__':
     app.run(debug=True)
