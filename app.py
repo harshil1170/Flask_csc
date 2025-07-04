1ca637ba9173dce33cc13772e31f5d2a2480e59f
Jamnesha Harshil <131521283+harshil1170@users.noreply.github.com>
2025-07-04 22:45:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("Home.html")
-    
-print("Current working directory:", os.getcwd())
 
 if __name__ == '__main__':
     app.run(debug=True)
